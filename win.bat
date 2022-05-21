9a87610c96649026ce8148980a50a5ffe6838555
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-05-21 17:05:08 +0530

    Update win.bat

--- win.bat
+++ win.bat
@@ -1,5 +1,5 @@
 md C:\dicli
 echo F|xcopy %~dp0\dicli.py C:\dicli\_dicli.py
-echo py _dicli.py>C:\dicli\dicli.bat
+echo py C:\dicli\_dicli.py>C:\dicli\dicli.bat
 setx /M path "%path%;C:\dicli"
 refreshenv
